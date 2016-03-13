dafad0eaf4a99f339f7bbd60c51aba78245e5d60
LiJunpeng <lijunpeng91@gmail.com>
2016-03-13 16:30:24 -0400

    modify angular module to make it execute immediately, and change 'z-index tab' to 'display:none/block tab'

--- js/ListController.js
+++ js/ListController.js
@@ -1,54 +1,52 @@
 
-var todo_list = angular.module('TodoListApp', []);
+var todo_list = angular.module('TodoListApp', [])
+  .controller('ListController', function($scope) 
+  {
 
-  todo_list.controller('ListController', function($scope) {
+    $scope.items = [
+      { detail: 'Get up', stat: 'Complete'},
+      { detail: 'Wash face', stat: 'Active'},
+      { detail: 'Eat breakfast', stat: 'Active'}
+    ];
 
-  $scope.items = [
-    { detail: 'Get up', stat: 'Complete'},
-    { detail: 'Wash face', stat: 'Active'},
-    { detail: 'Eat breakfast', stat: 'Active'}
-  ];
+    $scope.addItem = function () {
+      $scope.items.push(
+        {
+          detail: $scope.newItem.detail,
+          stat: 'Active'
+        });
+    }
 
-  $scope.addItem = function () {
-    $scope.items.push(
-      {
-        detail: $scope.newItem.detail,
-        stat: 'Active'
-      });
-  }
+    $scope.deleteItem = function (item) {
+      var index = $scope.items.indexOf(item);
+      $scope.items.splice(index, 1);     
+    }
 
-  $scope.deleteItem = function (item) {
-    var index = $scope.items.indexOf(item);
-    $scope.items.splice(index, 1);     
-  }
+    $scope.completeItem = function (item){
+      item.stat = 'Complete';
+    }
 
-  $scope.completeItem = function (item){
-    item.stat = 'Complete';
-  }
-
-});
-
+  });
 
 function changeTab(index)
 {
   switch(index)
   {
   case 1:
-    document.getElementById("all_list_wrapper").style.zIndex = 10;
-    document.getElementById("active_list_wrapper").style.zIndex = 0;
-    document.getElementById("complete_list_wrapper").style.zIndex = 0;
+    document.getElementById("all_list_wrapper").style.display = "block";
+    document.getElementById("active_list_wrapper").style.display = "none";
+    document.getElementById("complete_list_wrapper").style.display = "none";
     break;
   case 2:
-    document.getElementById("all_list_wrapper").style.zIndex = 0;
-    document.getElementById("active_list_wrapper").style.zIndex = 10;
-    document.getElementById("complete_list_wrapper").style.zIndex = 0;
+    document.getElementById("all_list_wrapper").style.display = "none";
+    document.getElementById("active_list_wrapper").style.display = "block";
+    document.getElementById("complete_list_wrapper").style.display = "none";
     break;
   case 3:
-    document.getElementById("all_list_wrapper").style.zIndex = 0;
-    document.getElementById("active_list_wrapper").style.zIndex = 0;
-    document.getElementById("complete_list_wrapper").style.zIndex = 10;
+    document.getElementById("all_list_wrapper").style.display = "none";
+    document.getElementById("active_list_wrapper").style.display = "none";
+    document.getElementById("complete_list_wrapper").style.display = "block";
     break;
   default:;
   }
 }
-
